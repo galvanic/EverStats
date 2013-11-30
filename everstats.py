ce164b078f10b10b68ca25771f17ecf2c6f42a98
Justine Courty <justinecourty@yahoo.fr>
2013-11-30 18:18:59 +0000

    Find total number of notes with a certain tag.

--- everstats.py
+++ everstats.py
@@ -8,6 +8,8 @@ For example:
 
 """
 
+import evernote.edam.notestore.ttypes as NoteStoreTypes
+
 from evernote.api.client import EvernoteClient
 
 
@@ -18,7 +20,44 @@ user = userStore.getUser()
 print user.username
 
 noteStore = client.get_note_store()
-notebooks = noteStore.listNotebooks()
-for n in notebooks:
-    print n.name, len(n)
+
+# notebooks = noteStore.listNotebooks()
+# for n in notebooks:
+#     print n.name
+
+# ###
+# nFilter = NoteStoreTypes.NoteFilter()
+# nFilter.words = getNonEmptyUserInput("Search for: ")
+# resultSpec = NoteStoreTypes.NotesMetadataResultSpec()
+# resultSpec.includeTitle = True
+
+# searchResults = note_store.findNotesMetadata(nFilter, 0, 10, resultSpec)
+
+# if len(searchResults.notes):
+#     for note in searchResults.notes:
+#         print note.guid
+#         print note.title
+# else:
+#     print "No results, big fella."
+###
+
+# get the guid of the tag we are looking for by searching for its name through a tag list
+list_tags = noteStore.listTags(dev_token)
+search_tag = "feminis"
+
+for i, t in enumerate(list_tags):
+    if search_tag in t.name.lower():
+        print "Found tag %s" % t.name
+        fem_guid = t.guid
+        break
+else:
+    print "No tag with '%s' in its name was found." % search_tag
+
+fem_filter = NoteStoreTypes.NoteFilter(tagGuids=[fem_guid])
+fem_notes  = noteStore.findNoteCounts(dev_token, fem_filter, False)
+
+total_fem = sum(fem_notes.notebookCounts.values())
+print total_fem
+
+
 
