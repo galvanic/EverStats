56b076c35acb20e4318227eff8c3e5c2b30e9f7b
Justine Courty <justinecourty@yahoo.fr>
2013-11-30 18:38:34 +0000

    Put the filter by tag feature neatly into a function.

--- everstats.py
+++ everstats.py
@@ -41,23 +41,42 @@ noteStore = client.get_note_store()
 #     print "No results, big fella."
 ###
 
-# get the guid of the tag we are looking for by searching for its name through a tag list
-list_tags = noteStore.listTags(dev_token)
-search_tag = "feminis"
+def filterByTag(tagname, wordy=False):
+    """
+    Returns a list of tag GUIDS corresponding to tag names inputed (string).
+    Only returns one tag for the moment, might make it do more later.
+    """
+    list_tags = noteStore.listTags(dev_token)
 
-for i, t in enumerate(list_tags):
-    if search_tag in t.name.lower():
-        print "Found tag %s" % t.name
-        fem_guid = t.guid
-        break
-else:
-    print "No tag with '%s' in its name was found." % search_tag
+    for t in list_tags:
+        if tagname in t.name.lower():
+            if wordy:
+                print "Found tag %s" % t.name
+            wanted_tag = t.guid
+            break
+    else:
+        print "No tag with '%s' in its name was found." % tagname
+    return [wanted_tag]
 
-fem_filter = NoteStoreTypes.NoteFilter(tagGuids=[fem_guid])
+
+fem_filter = NoteStoreTypes.NoteFilter(tagGuids=filterByTag("feminis"))
 fem_notes  = noteStore.findNoteCounts(dev_token, fem_filter, False)
 
 total_fem = sum(fem_notes.notebookCounts.values())
 print total_fem
+
+
+# But that's not what I was looking for :(
+# I want to find the actual notes with this tag, so that I can get their createdDate and plot them
+
+
+
+
+
+
+
+
+
 
 
 
